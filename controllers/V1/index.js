589543670549792656e1a1b39c87f02fb14a489b
elouadi <slimane.el-ouadi@etudiant.univ-lille1.fr>
2015-09-11 11:03:50 +0200

    ajout des routes V1

--- controllers/V1/index.js
+++ controllers/V1/index.js
@@ -8,4 +8,30 @@ router.get('/', function(req, res, next) {
   response.send(res,contenu);
 });
 
+
+
+// on récupère toutes les catégories = themes
+router.get('/categories', function (req, res, next) {
+   var contenu = {categories : 'list of categories'};
+   response.send(res,contenu);
+});
+
+// on récupère tous les services existants dans la base de données
+router.get('/services', function (req, res, next) {
+   var contenu = {services : 'list of services'};
+   response.send(res,contenu);
+});
+
+// on récupère toutes les activités 
+router.get('/activities', function (req, res, next) {
+   var contenu = {activities : 'list of activities'};
+   response.send(res,contenu);
+});
+
+// on récupère toutes les informations associées à un service
+router.get('/services/:id', function (req, res, next){
+	var contenu = {service : 'description of one service'};
+    response.send(res,contenu);
+});
+
 module.exports = router;

c25afd5d924268374c965f52a08eda7216e0da3e
Romain SOMMERARD <rsommerard@gmail.com>
2015-09-21 11:40:49 +0200

    build(api): Add grunt
    Add grunt to be abble to test project.

--- controllers/V1/index.js
+++ controllers/V1/index.js
@@ -1,37 +1,35 @@
 var express = require('express');
-var response = require('../../models/response_m');
+var response = require('../../models/response_message');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var contenu = {version : 'v1'};
-  response.send(res,contenu);
+router.get('/', function(req, res) {
+  var content = {version: 'v1'};
+  res.status(200).send(content);
 });
 
-
-
 // on récupère toutes les catégories = themes
-router.get('/categories', function (req, res, next) {
-   var contenu = {categories : 'list of categories'};
-   response.send(res,contenu);
+router.get('/categories', function(req, res) {
+  var content = {categories: 'list of categories'};
+  res.status(200).send(content);
 });
 
 // on récupère tous les services existants dans la base de données
-router.get('/services', function (req, res, next) {
-   var contenu = {services : 'list of services'};
-   response.send(res,contenu);
+router.get('/services', function(req, res) {
+  var content = {services: 'list of services'};
+  res.status(200).send(content);
 });
 
-// on récupère toutes les activités 
-router.get('/activities', function (req, res, next) {
-   var contenu = {activities : 'list of activities'};
-   response.send(res,contenu);
+// on récupère toutes les activités
+router.get('/activities', function(req, res) {
+  var content = {activities: 'list of activities'};
+  res.status(200).send(content);
 });
 
 // on récupère toutes les informations associées à un service
-router.get('/services/:id', function (req, res, next){
-	var contenu = {service : 'description of one service'};
-    response.send(res,contenu);
+router.get('/services/:id', function(req, res) {
+  var content = {service: 'description of one service'};
+  res.status(200).send(content);
 });
 
 module.exports = router;

b86506b8d2c15cd2b7d86cb47b872f1dea927411
Romain SOMMERARD <rsommerard@gmail.com>
2015-09-30 08:40:01 +0200

    Add db config

--- easytrip/config/env/development.js
+++ easytrip/config/env/development.js
@@ -20,5 +20,8 @@ module.exports = {
   // models: {
   //   connection: 'someMongodbServer'
   // }
-
+  models: {
+    connection: 'localDiskDb'
+  },
+  migrate: 'drop'
 };
--- easytrip/config/env/production.js
+++ easytrip/config/env/production.js
@@ -12,6 +12,10 @@
 
 module.exports = {
 
+  models: {
+    connection: 'postgresqlServer'
+  }
+
   /***************************************************************************
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *

26e9af7a1913cc6cea38919e54c2c9310f435f59
Romain SOMMERARD <rsommerard@gmail.com>
2015-09-30 08:05:15 +0200

    Test dev and prod db switching

--- sails-playground/testProject/config/env/production.js
+++ sails-playground/testProject/config/env/production.js
@@ -17,9 +17,9 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'somePostgresqlServer'
+  }
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *

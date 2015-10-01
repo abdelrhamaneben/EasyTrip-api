892bcf526178f2bd1d2abc896ab51f46013185c0
abdelrhamane <abdelrhamane@hotmail.fr>
2015-10-01 21:48:18 +0200

    Add data Test

--- api/models/Category.js
+++ api/models/Category.js
@@ -11,17 +11,22 @@ module.exports = {
 		 id_category : {
 		 	type : 'integer',
 		 	primaryKey: true,
-		 	autoIncrement: true,
-	    required: true
+		 	autoIncrement: true
 		 },
 		 name : {
 		 	type : 'string',
-		 	size : 50
+		 	size : 50,
+	    	required: true
 		 },
 		 description : {
 		 	type : 'string',
-		 	size : 200
-		 }
+		 	size : 200,
+		 	required: true
+		 },
+	     activities:{
+	            collection: 'activity',
+	            via: 'categories'
+	     }
   }
 };
 

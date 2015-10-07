b98c3cc285017de70c62532919fc0f6046e670e6
Romain SOMMERARD <rsommerard@gmail.com>
2015-10-07 11:47:00 +0200

    Add location recover and time calculation between 2 points

--- api/controllers/ServiceController.js
+++ api/controllers/ServiceController.js
@@ -6,6 +6,46 @@
  */
 
 module.exports = {
-	
-};
 
+  search: function (req, res) {
+    var geocoder = require('geocoder');
+    var request = require('request');
+
+    function getOriginDestinationInfo() {
+      var origin = req.param('origin');
+      var destination = req.param('destination');
+      console.log(origin);
+      console.log(destination);
+
+      geocoder.geocode(origin, function ( err, data ) {
+        originLocation = data.results[0].geometry.location;
+        console.log("originLocation: " + originLocation.lat + ',' + originLocation.lng);
+
+        geocoder.geocode(destination, function ( err, data ) {
+          destinationLocation = data.results[0].geometry.location;
+          console.log("destinationLocation: " + destinationLocation.lat + ',' + destinationLocation.lng);
+
+          console.log(">originLocation: " + originLocation.lat + ',' + originLocation.lng);
+          console.log(">destinationLocation: " + destinationLocation.lat + ',' + destinationLocation.lng);
+
+          var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=';
+          url += originLocation.lat + ',' + originLocation.lng;
+          url += '&destinations=';
+          url += destinationLocation.lat + ',' + destinationLocation.lng;
+          url += '&language=fr-FR'
+
+          request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(body) // Show the HTML for the Google homepage.
+
+              return res.send(body);
+            }
+          });
+        });
+      });
+    };
+
+    getOriginDestinationInfo();
+
+  }
+};
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  "POST /search": {controller: "ServiceController", action: "search"}
 
   /***************************************************************************
   *                                                                          *

1b8fab3f043026e047753ccb21e71309eb7dd655
Romain SOMMERARD <rsommerard@gmail.com>
2015-11-18 09:07:01 +0100

    Fix deprecated

--- config/express.js
+++ config/express.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 
-module.exports.express = {
+module.exports.http = {
     customMiddleware: function (app) {
         app.use(passport.initialize());
         app.use(passport.session());

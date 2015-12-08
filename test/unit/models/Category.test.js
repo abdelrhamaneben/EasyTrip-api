2f29b42150cc15a3ada465f1b3cb925ba90395c9
abdelrhamane <abdelrhamane@hotmail.fr>
2015-12-08 13:13:54 +0100

    add(view): model feature view to comment and to show informations ()

--- test/unit/models/Category.test.js
+++ test/unit/models/Category.test.js
@@ -2,11 +2,8 @@ describe('Category', function() {
   it ('should not be empty', function(done) {
     Category.find().exec(function(err, category) {
       category.length.should.not.be.eql(0);
+      console.log(Category);
       done();
     });
-  });
-
-  it ('should not be empty', function(done) {
-    
   });
 });

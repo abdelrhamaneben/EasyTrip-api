951cb31a48680ff6dd619c982bbb4974c79955c1
jvdurieu <jvdurieu@live.be>
2016-01-13 11:33:31 +0100

    Last unfinished updates, because abdel ;)

---
+++ api/controllers/NotificationController.js
@@ -0,0 +1,108 @@
+
+module.exports = {
+
+  getStats: function(req, res) {
+/*
+    var notifs = [
+      // TIME : timestamp from 01011970
+      // SOURCE : [comment, eval, newService, newActivity, newPromo, endPromo]
+      // VALUE : []
+       { "A": value, "B": value, ... },
+       { "A": value, "B": value, ... },
+       ...
+     ]
+
+
+
+        <a href="#" class="list-group-item">
+            <i class="fa fa-comment fa-fw"></i> Ajout activite SAUT EN PARACHUTE
+            <span class="pull-right text-muted small"><em>4 minutes ago</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-twitter fa-fw"></i> Ajout categorie SPORT EXTREME
+            <span class="pull-right text-muted small"><em>12 minutes ago</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-envelope fa-fw"></i> Modification du service RUGBY LILL1 dans RUGBY dans SPORT
+            <span class="pull-right text-muted small"><em>27 minutes ago</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-tasks fa-fw"></i> Nouveau service RUGBY LILL1 dans RUGBY dans SPORT
+            <span class="pull-right text-muted small"><em>43 minutes ago</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-upload fa-fw"></i> Modification activite MUSE
+            <span class="pull-right text-muted small"><em>11:32 AM</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-bolt fa-fw"></i> ajout activite MUSE dans CULTURE
+            <span class="pull-right text-muted small"><em>11:13 AM</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-warning fa-fw"></i> ajout categorie CULTURE
+            <span class="pull-right text-muted small"><em>10:57 AM</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-shopping-cart fa-fw"></i> ajout activite RUGBY dans SPORT
+            <span class="pull-right text-muted small"><em>9:49 AM</em>
+            </span>
+        </a>
+        <a href="#" class="list-group-item">
+            <i class="fa fa-money fa-fw"></i> Ajout categorie SPORT
+            <span class="pull-right text-muted small"><em>Yesterday</em>
+            </span>
+        </a>
+
+
+
+    // Sample réponse
+
+     // Nombre de visites
+    stats.nbvue7d = 31;
+    stats.nbvue15d = 52;
+    stats.nbvue30d = 67;
+    stats.nbvue45d = 87;
+    stats.nbvue60d = 92;
+    stats.nbvue75d = 101;
+    stats.nbvue90d = 101;
+
+    // Nombre de recherches
+    stats.nbcmt7d = 2;
+    stats.nbcmt15d = 7;
+    stats.nbcmt30d = 14;
+    stats.nbcmt45d = 23;
+    stats.nbcmt60d = 26;
+    stats.nbcmt75d = 33;
+    stats.nbcmt90d = 38;
+
+    // nombre de création de compte utilisateur Util & Business
+    stats.nveval7d = 3;
+    stats.nveval15d = 13;
+    stats.nveval30d = 20;
+    stats.nveval45d = 32;
+    stats.nveval60d = 41;
+    stats.nveval75d = 60;
+    stats.nveval90d = 67;
+
+    stats.eval7d = 4.4;
+    stats.eval15d = 4.3;
+    stats.eval30d = 4.0;
+    stats.eval45d = 4.3;
+    stats.eval60d = 4.2;
+    stats.eval75d = 4.1;
+    stats.eval90d = 3.8;
+
+    res.ok(stats);
+*/
+  }
+
+
+
+};

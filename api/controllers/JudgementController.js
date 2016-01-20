368f6bc120eb12508bb35a32bf40fccc4b3e43ed
Tello Anis <tello@b10p11.fil.univ-lille1.fr>
2016-01-20 11:39:30 +0100

    test JudgementController

---
+++ api/controllers/JudgementController.js
@@ -0,0 +1,10 @@
+/**
+ * JudgementController
+ *
+ * @description :: Server-side logic for managing Services
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};

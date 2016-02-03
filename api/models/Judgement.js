28cacb887fc22a44c39269a5adbdd80bbed82939
Romain SOMMERARD <rsommerard@gmail.com>
2016-02-03 09:20:40 +0100

    Add judgement table

---
+++ api/models/Judgement.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+  tableName: 'et_judgement',
+
+  attributes: {
+    id_judgement: {
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    id_user: {
+      model: 'user'
+    },
+    id_service: {
+      model: 'service'
+    },
+    judgement: {
+      type: 'string',
+      size: 500
+    },
+    global_score: {
+      type: 'integer'
+    },
+    score1: {
+      type: 'integer'
+    },
+    score2: {
+      type: 'integer'
+    },
+    score3: {
+      type: 'integer'
+    }
+  }
+};
